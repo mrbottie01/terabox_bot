3474517b32fcb40c4b0c152209d42c1079264a6d
mrbottie01 <dbdbmohan@gmail.com>
2025-08-06 13:28:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I’m your Terabox Bot!")
+    await update.message.reply_text("Bot is alive and ready, Commander.")
 
-def main():
+if __name__ == "__main__":
+    if not TOKEN:
+        raise Exception("BOT_TOKEN not found in environment.")
+    
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
-
-if __name__ == "__main__":
-    main()
